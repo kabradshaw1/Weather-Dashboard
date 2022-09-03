6e621b7789f0ac8100134c0143d577ecb5ffea0e
kabradshaw1 <itllbok82@gmail.com>
2022-09-03 17:54:51 -0400

    local starge working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,34 @@ var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#cityname");
 var todaycontainer = document.querySelector("#weather-container");
 var cityButtonsEl = document.querySelector("#city-buttons");
+var buttons = [];
+
+var saveButtons = function () {
+    localStorage.setItem("buttons", JSON.stringify(buttons));
+};
+
+var loadButtons = function() {
+    var savedButtons = localStorage.getItem("buttons");
+    // if there are no buttons saved, set buttons to an empty array and return of the funtion
+    if(!savedButtons) {
+        return false;
+    };
+
+    // else, load up buttons
+
+    // parse into array of objects
+    savedButtons = JSON.parse(savedButtons);
+
+    // loop through savedButtons array
+    for (var i = 0; i < savedButtons.length; i++) {
+        // pass each button object to createButton() function
+        createButton(savedButtons[i]);
+    }
+};
+
+var getLatLon = function (submit) {
+    
+};
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -12,7 +40,8 @@ var formSubmitHandler = function(event) {
   
     if (cityname) {
     //   getWeather(cityname);
-        console.log(cityname);
+        createButton(cityname);
+        // getLatLon();
   
       // clear old content
       cityInputEl.value = "";
@@ -21,20 +50,23 @@ var formSubmitHandler = function(event) {
     }
 };
 
-var createButton = function (CityObj) {
+var createButton = function (cityObj) {
     // create button
     var cityEl = document.createElement("button");
     // for styling
     var buttonHolder = document.querySelector("#history-buttons");
     cityEl.className = "btn btn-secondary";
-    cityEl.innerHTML
+    cityEl.innerHTML = cityObj;
+
 
     // going to need to add something here that will specificy what
     // it's going to search for when it's clicked
 
     // append to <div id="history-buttons">
-    buttonHolder.appendChild(CityEl);
-
+    buttonHolder.appendChild(cityEl);
+    buttons.push(cityObj);
+    console.log(buttons);
+    saveButtons();
 }
 
 var getWeather = function(user) {
@@ -72,3 +104,5 @@ var getWeather = function(user) {
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
 // cityButtonsEl.addEventListener("click", buttonClickHandler);
+
+loadButtons()
